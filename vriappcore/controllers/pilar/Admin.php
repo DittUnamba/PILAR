b780ba712ba566f58ac136973b7dcea663885974
johnel7 <nhelrojas00@gmail.com>
2023-12-05 16:33:22 -0500

    Modificacion de variable en archivo admin

--- vriappcore/controllers/pilar/Admin.php
+++ vriappcore/controllers/pilar/Admin.php
@@ -108,7 +108,7 @@ class Admin extends CI_Controller {
     }
     */
 
-    private function logCorreo( $idUser, $correo, $, $mensaje )
+    private function logCorreo( $idUser, $correo, $titulo, $mensaje )
     {
         if( !$correo ) return;
 
